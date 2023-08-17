a3593ec3076b2c68e718c29acc9b6108d0db8ce9
Akinyemi Taiwo Hassan <akinyemitaiwo646.com>
2023-08-17 14:40:07 -0700

    Initial commit

---
+++ countdown.js
@@ -0,0 +1,82 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const giveaway=document.querySelector('.giveaway');
+  const deadline=document.querySelector('.deadline');
+  const items=document.querySelectorAll('.deadline-format h4');
+// console.log(items); 
+let futureDate=new Date(2020, 3, 26, 1, 30, 0);
+// console.log(futureDate);
+const year=futureDate.getFullYear();
+const hours=futureDate.getHours();
+const minutes=futureDate.getMinutes();
+
+
+let month=futureDate.getMonth();
+month=months[month];
+const date=futureDate.getDate();
+
+const weekday=weekdays[futureDate.getDay()];
+
+
+giveaway.textContent=`giveaway ends on ${weekday}, ${month} ${year} ${hours}: ${minutes}`;
+// console.log(weekday);
+//future time in ms
+const futureTime=futureDate.getTime();
+console.log(futureTime);
+
+
+function getRemainingTime(){
+  const today=new Date().getTime();
+  const t=futureTime-today;
+  console.log(t);
+  //1s=1000ms
+  //1m=60s
+  //1hr=60min
+  //1day=24hrs
+  //values in ms
+  const OneDay=24*60*60*1000;
+  const oneHour=60*60*1000;
+  const oneMinute=60*1000;
+  //calculate all values
+  let days=t/OneDay
+  // console.log(OneDay);
+  // console.log(days);
+  days=Math.floor(days);
+  let hours=Math.floor((t % OneDay)/oneHour);
+  let minutes=Math.floor((t % oneHour)/oneMinute);
+  let seconds=Math.floor((t % oneMinute)/1000);
+  //set values  array:
+  const values=[days,hours,minutes,seconds];
+
+  items.forEach(function(item,index){
+    item.innerHTML=values[index];
+
+  });
+
+  // console.log(hours);
+}
+getRemainingTime();
+
+
+  q
---
+++ counter.md
@@ -0,0 +1,31 @@
+Counter App
+The Counter App is a simple and intuitive web application built using HTML, CSS, and JavaScript. This app provides a straightforward way to keep track of counts, whether you're tallying votes, tracking inventory, or any other scenario where counting is essential.
+
+Features
+User-Friendly Interface: The Counter App features a clean and user-friendly interface that makes it easy for users to increase, decrease, and reset counts.
+
+Increment and Decrement: Users can increment or decrement the count by simply clicking the "+" and "-" buttons, respectively.
+
+Reset: The app allows users to reset the count back to zero with a single click.
+
+
+Customization
+You can easily customize the Counter App's appearance and behavior by modifying the included CSS and JavaScript files. Feel free to adapt the styling, colors, and even add more features to suit your specific needs.
+
+Technologies Used
+HTML: For structuring the app's layout and content.
+CSS: For styling and visual enhancements.
+JavaScript: For implementing the interactive features and count management.
+License
+This project is licensed under the MIT License, which means you can use, modify, and distribute the code freely. See the LICENSE file for more details.
+
+Contact
+If you have any questions, suggestions, or feedback, feel free to contact us at akinyemitaiwo646@gmail.com
+
+Happy counting! 📊
+
+
+
+
+
+
